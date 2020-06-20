a757687e53954bc6fcbdca38f7045a6f463426b6
Aditya Jain <vvasujain@gmail.com>
2020-06-20 16:46:19 +0530

    Final Release

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+define('WP_USE_THEMES', true);
+require( dirname( __FILE__ ) . '/wordpress/wp-blog-header.php' );
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
